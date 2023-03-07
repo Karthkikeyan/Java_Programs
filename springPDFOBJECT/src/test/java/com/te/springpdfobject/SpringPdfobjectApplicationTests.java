8451073a4215ba0bdd326f3988b0f3be72b2486e
Karthkikeyan <121166980+Karthkikeyan@users.noreply.github.com>
2023-03-07 12:33:24 +0530

    Grooming Topic-PDF Generator

---
+++ springPDFOBJECT/src/test/java/com/te/springpdfobject/SpringPdfobjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.te.springpdfobject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPdfobjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
